f1fb31a57e40e11480492fbdc531a0c54460be7c
Aljun2007 <1171682005@qq.com>
2024-08-08 10:25:00 +0800

    构建

--- src/main/java/com/aljun/uninfectedzone/common/sound/UninfectedZoneSounds.java
+++ src/main/java/com/aljun/uninfectedzone/common/sound/UninfectedZoneSounds.java
@@ -16,9 +16,9 @@ public class UninfectedZoneSounds {
 
     public static void register(IForgeRegistry<SoundEvent> registry) {
         CUSTOM_ZOMBIE_HURT = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.hurt")), "custom_zombie_hurt");
-        CUSTOM_ZOMBIE_AMBIENT = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.ambient")), "custom_zombie_hurt");
+        CUSTOM_ZOMBIE_AMBIENT = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.ambient")), "custom_zombie_ambient");
         CUSTOM_ZOMBIE_DEATH = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.death")), "custom_zombie_death");
-        CUSTOM_ZOMBIE_STEP = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.hurt")), "custom_zombie_hurt");
+        CUSTOM_ZOMBIE_STEP = register(registry, () -> new SoundEvent(new ResourceLocation(UninfectedZone.MOD_ID, "entity.custom_zombie.step")), "custom_zombie_step");
     }
 
     private static Supplier<SoundEvent> register(IForgeRegistry<SoundEvent> registry, Supplier<SoundEvent> instance, String key) {
@@ -27,3 +27,4 @@ public class UninfectedZoneSounds {
         return () -> soundEvent;
     }
 }
+
